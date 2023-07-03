eb487e08914b9116ee3280ee9e049d074d10aee8
Jon Bråte <jon.brate@fhi.no>
2023-07-03 12:03:31 +0200

    Using factors

--- scripts/samples_per_week.R
+++ scripts/samples_per_week.R
@@ -2,6 +2,25 @@ library(tidyverse)
 library(lubridate)
 library(jsonlite)
 
+#### THOUGHTS ####
+# Maybe don't always include all months and all years etc.
+# Only include what we have data for 
+# Then it will be important to generate the category file from the actual data
+
+# Create the data structure that will be filled with values. Using strings only to get factors later in crossing()
+month_numbers <- as.character(1:12)
+fylke_names   <- c("Agder", "Innlandet", "Møre og Romsdal", "Nordland", "Oslo", "Rogaland", "Troms og Finnmark", "Trøndelag", "Vestfold og Telemark", "Vestland", "Viken", "Totalt")
+years         <- c("2022", "2023")
+
+# Create all possible combinations of fylke number, fylke name and year
+final_data <- crossing(month_numbers, years, fylke_names) %>% 
+  # Create new factor columns
+  rename(
+    "MONTH"  = "month_numbers",
+    "YEAR"   = "years",
+    "FYLKE"  = "fylke_names"
+  )
+
 # Les alle utslusningsfilene fra LabWare
 files <- list.files(path = "N:/Virologi/Influensa/2223/LabwareUttrekk/", 
                     pattern = "csv$",
@@ -58,41 +77,77 @@ df <- df %>%
 fylke_month <- df %>% 
   group_by(MONTH, YEAR, Fylke) %>% 
   count() %>% 
+  ungroup() %>% 
   # Remove empty Fylkenavn
   filter(str_detect(Fylke, "")) %>% 
   # Some Fylke are named zero
   filter(Fylke != "0") %>% 
   # Drop empty year info
-  filter(!is.na(YEAR))
+  filter(!is.na(YEAR)) %>% 
+  rename("FYLKE" = "Fylke") %>% 
+  mutate(MONTH = as.character(MONTH),
+         YEAR = as.character(YEAR))
 
 # Calculate samples per month per year for all Fylker
 total_month <- df %>% 
   group_by(MONTH, YEAR) %>% 
   count() %>% 
+  ungroup() %>% 
   add_column("Fylke" = "Totalt") %>% 
   # Drop empty year info
-  filter(!is.na(YEAR))
+  filter(!is.na(YEAR)) %>% 
+  rename("FYLKE" = "Fylke") %>% 
+  mutate(MONTH = as.character(MONTH),
+         YEAR = as.character(YEAR))
 
 # Combine data
-data <- bind_rows(fylke_month, total_month) %>% 
-  # Complete series
-  ungroup() %>% 
-  complete(MONTH, YEAR, Fylke)
+data <- bind_rows(fylke_month, total_month) 
+
+# Join with final data structure to get all possible combinations of month, year and fylke
+final_data <- left_join(final_data, data) %>% 
+  rename("ANTALL" = "n") %>% 
+  # Create new factor columns
+  mutate(
+    "MONTH"  = factor(MONTH),
+    "YEAR"   = factor(YEAR),
+    "FYLKE"  = factor(FYLKE)
+  )
+
 
 # Add flags for FHI Statistikk
-data <- data %>% 
+final_data <- final_data %>% 
   add_column("SPVFLAGG" = 0)
 
 # Replace NA with 0
-data <- data %>% 
-  mutate(n = replace_na(n, 0)) 
+final_data <- final_data %>% 
+  mutate(ANTALL = replace_na(ANTALL, 0)) 
 
 # Write as csv
-write_csv(data, file = "data/SC2_samples/seqs_per_month.csv")
+write_csv(final_data, file = "data/SC2_samples/seqs_per_month.csv")
 
 
 # Create the category file
 
+# First creating a list with the different levels necessary
+
+# Need three top levels: 
+## Dimensions
+### [[1]]
+#### Value
+#### Title
+#### Categories
+
+### [[2]]
+### [[3]]
+## MeasureTypes
+## Flags
+
+# Read example file
+example <- read_json("data/SC2_samples/seqs_per_month.category.json")
+
+my_list <- list()
+
+list(levels(final_data$YEAR))
 
 {
   "Dimensions": [
