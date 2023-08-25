412c8300bbdd5be546ec246050c6a8cc50e93a64
Jon Bråte <jon.brate@fhi.no>
2023-08-25 15:19:22 +0200

    Added file

---
+++ scripts/variants_per_month.R
@@ -0,0 +1,460 @@
+library(tidyverse)
+library(lubridate)
+
+
+# Set up data structures --------------------------------------------------
+
+# Create the data structure that will be filled with values. Using strings only to get factors later in crossing()
+month_numbers <- as.character(1:12)
+month_names   <- lubridate::month(1:12, label = T, abbr = T, locale = "Norwegian_Norway") 
+years         <- c("2023")
+
+# Create all possible combinations of month and year 
+final_data <- crossing(month_names, years) %>% 
+  rename(
+    "MONTH"  = "month_names",
+    "YEAR"   = "years"
+  ) %>% 
+  # Create YEARMONTH column for combining later
+  unite("YEARMONTH", c("YEAR", "MONTH"), sep = "-", remove = T) %>% 
+  mutate(YEARMONTH = as.factor(YEARMONTH))
+
+
+# Get data from BN. Needs to be changed from Andreas ----------------------
+
+con <- odbc::dbConnect(odbc::odbc(),
+                 Driver = "SQL Server",
+                 Server = "sql-bn-covid19",
+                 Database = "BN_Covid19")
+
+BN <- dplyr::tbl(con, "ENTRYTABLE") %>% 
+  # Keep all columns
+  #select(KEY, REKVNR, PROVE_TATT, FYLKENAVN, MATERIALE, PROSENTDEKNING_GENOM, DEKNING_NANOPORE, SEKV_OPPSETT_NANOPORE, DEKNING_NANOPORE, SEKV_OPPSETT_SWIFT7,
+  #       SEQUENCEID_NANO29, SEQUENCEID_SWIFT, COVERAGE_BREADTH_SWIFT, GISAID_PLATFORM, GISAID_EPI_ISL, GENOTYPE_SVART_I_LABWARE, COVERAGE_BREATH_EKSTERNE,
+  #       SAMPLE_CATEGORY, INNSENDER, COVERAGE_DEPTH_SWIFT, COVARAGE_DEPTH_NANO, RES_CDC_INFA_RX, RES_CDC_INFB_CT, MELDT_SMITTESPORING, PANGOLIN_NOM,
+  #       ENA_RUN, ENA_BIOSAMPLE, GISAID_ISOLAT_NAVN) %>%
+  rename("Dekning_Artic" = PROSENTDEKNING_GENOM,
+         "Dekning_Swift" = COVERAGE_BREADTH_SWIFT,
+         "Dekning_Nano" = DEKNING_NANOPORE,
+         "Dekning_Eksterne" = COVERAGE_BREATH_EKSTERNE) %>% 
+  # Collect will actually retrieve the data and store it as a local tibble
+  collect()
+
+# Convert comma to dot in the coverage
+BN <- BN %>% 
+  # Replace a few double commas
+  mutate(Dekning_Nano = str_replace(Dekning_Nano, ",,", ",")) %>% 
+  # Change comma to decimal for the coverage
+  mutate(Dekning_Artic = str_replace(Dekning_Artic, ",", "."),
+         Dekning_Swift = str_replace(Dekning_Swift, ",", "."),
+         Dekning_Nano = str_replace(Dekning_Nano, ",", "."),
+         Dekning_Eksterne = str_replace(Dekning_Eksterne, ",", "."))
+
+# Filter the data
+tmp <- BN %>% 
+  # Fjerne evt positiv controll
+  filter(str_detect(KEY, "pos", negate = TRUE)) %>%
+  # Keep samples with a valid Pango
+  filter(!is.na(PANGOLIN_NOM)) %>%
+  # Remove a few weird Pangos
+  filter(str_detect(PANGOLIN_NOM, "BESTILT", negate = T)) %>% 
+  filter(str_detect(PANGOLIN_NOM, "konklusiv", negate = T)) %>% 
+  filter(str_detect(PANGOLIN_NOM, "Unassigned", negate = T)) %>% 
+  filter(str_detect(PANGOLIN_NOM, "kommentar", negate = T)) %>% 
+  filter(PANGOLIN_NOM != "") %>% # Remove empty string for Pango
+  # Fix date format
+  mutate("PROVE_TATT" = ymd(PROVE_TATT)) %>% 
+  # Keep samples from 2023 only
+  filter(PROVE_TATT > "2022-12-31") %>% 
+  # select relevant columns
+  select(PROVE_TATT, PANGOLIN_NOM, FULL_PANGO_LINEAGE)
+
+# Create year and month columnd
+tmp <- tmp %>% 
+  mutate(YEAR = year(PROVE_TATT),
+         MONTH = month(PROVE_TATT, label = T, abbr = T, locale = "Norwegian_Norway")) %>% 
+  unite("YEARMONTH", c(YEAR, MONTH), sep = "-", remove = FALSE) %>% 
+  mutate(YEARMONTH = factor(YEARMONTH))
+  
+
+# Calculate variant frequency ---------------------------------------------
+
+freqs <- tmp %>% 
+  # Count number of lineages per month per year
+  count(PANGOLIN_NOM, YEARMONTH) %>% 
+  # Add total number of sequences per month per year
+  group_by(YEARMONTH) %>% 
+  mutate("Total_seqs_pr_month" = sum(n)) %>% 
+  ungroup() %>% 
+  # Calculate percentages
+  mutate(Percent = round(100 * n / Total_seqs_pr_month, 2)) %>% 
+  arrange(desc(Percent))
+  
+# Pull out current and previous month
+freqs %>% 
+  filter(YEARMONTH == "2023-aug" | YEARMONTH == "2023-jul") %>% 
+  select(PANGOLIN_NOM, YEARMONTH, Percent) %>% 
+  pivot_wider(names_from = YEARMONTH, values_from = Percent) %>% 
+  arrange(desc(`2023-aug`)) %>% 
+  # Calculate percent change
+  mutate(change = (`2023-jul` - `2023-aug`) / `2023-jul` * 100)
+
+
+# DELETE ------------------------------------------------------------------
+
+
+
+
+# MASSERT UTSLUSNINGSFIL LAGET I SIKKER SONE ------------------------------
+
+# Les filen
+df <- read_tsv("data/SC2_samples/2023.08.15-SARS-CoV-2_samples_for_FHI_Statistikk.tsv.zip")
+
+# Denne filen inneholder alle SARS-CoV-2-prøver som har "Test_status" "A". 
+# ToDo: Er alle disse relevante å ta med? Noen som skal ut?
+# TODO: bør sjekke om det er noen fylkenavn som ikke er med i listen over
+# TODO: Trenger vi dele inn i aldersgruppe? Blir fort for små tall...
+
+# Hva er det jeg trenger å ta med videre?
+# Kanskje enklere å trekke ut info separat og binde sammen senere enn å pivotere først?
+
+# I MSIS i dag på FHI Statistikk kan man filtrere på Aldersgruppe, Kjønn, Fylke, og Måned (i tillegg til smittestd Norge eller utlandet).
+# Alt dette bør derfor være regnet ut og tilgjengelig i samme csv-fil.
+
+# Først regne ut antall prøver per måned per fylke 
+# Denne informasjonen står i alle radene 
+
+# Date formats have changed at some point. Harmonize this and merge before further processing
+dates_1 <- df %>% 
+  # Pull out dates om day.month.year format
+  filter(str_detect(Sample_date, "\\.")) %>% 
+  # Convert to year-month-day
+  mutate(Sample_date = lubridate::dmy_hms(Sample_date))
+
+dates_2 <- df %>% 
+  # Pull out dates om day.month.year format
+  filter(str_detect(Sample_date, "-")) %>% 
+  # Convert to year-month-day
+  mutate(Sample_date = lubridate::ymd(Sample_date)) 
+
+dates <- rbind(dates_1, dates_2)
+
+df_mod <- dates %>% 
+  select("PROVE_TATT" = Sample_date, 
+         "FYLKE" = Fylke, 
+         "FODT_AAR" = Born_year, 
+         "FODT_MND" = Born_month, 
+         "KJONN" = Gender) %>% 
+  # Remove identical rows
+  distinct() %>% 
+  # Rename NA til "Ukjent fylke" and convert to factor
+  mutate(FYLKE = factor(replace_na(FYLKE, "Ukjent fylke"))) %>% 
+  # Drop samples with no date 
+  filter(!is.na(PROVE_TATT)) %>% 
+  # Add year and month columns
+  mutate(MONTH = month(PROVE_TATT),
+         month_name = month(PROVE_TATT, label = T, abbr = T, locale = "Norwegian_Norway"),
+         YEAR = year(PROVE_TATT)) %>% 
+  unite("YEARMONTH", c(YEAR, month_name), sep = "-", remove = FALSE) %>% 
+  mutate(YEARMONTH = factor(YEARMONTH)) %>% 
+  # Remove samples with wrongly formatted dates
+  filter(!is.na(PROVE_TATT))
+
+# Calculate samples per month per year per Fylke and total for the country
+fylke_month <- df_mod %>% 
+  #count(MONTH, YEAR, Fylke) %>% 
+  count(YEARMONTH, FYLKE) %>% 
+  rename("ANTALL" = "n")
+
+# Calculate samples per month per year for all Fylker
+total_month <- df_mod %>% 
+  count(YEARMONTH) %>% 
+  rename("ANTALL" = "n") %>% 
+  add_column("FYLKE" = "Totalt") %>% 
+  select(YEARMONTH, FYLKE, ANTALL)
+
+# Calculate per age group per fylke per month per year
+
+# Combine data
+data <- bind_rows(fylke_month, total_month) 
+
+# Join with final data structure to get all possible combinations of month, year and fylke
+
+final_data <- left_join(final_data, data, by = join_by("YEARMONTH", "FYLKE")) %>% 
+  # Create new  columns
+  separate(YEARMONTH, into = c("YEAR", "MONTH"), sep = "-", remove = T) %>% 
+  mutate(YEAR = as.factor(YEAR),
+         MONTH = as.factor(MONTH),
+         FYLKE = as.factor(FYLKE)) %>% 
+  # Add flags for FHI Statistikk
+  add_column("SPVFLAGG" = 0) %>% # 0 er default og betyr at verdien finnes i tabellen
+  mutate(SPVFLAGG = case_when(
+    is.na(ANTALL) ~ 1, # Sett SPVFLAGG til 1 hvis NA. Altså at vi ikke har samlet inn data for denne perioden
+    ANTALL < 5 ~ 3, # Vi setter alle verdier lavere enn 5 til 0
+    .default = 0
+  )) %>% 
+  # Replace NA with 0
+  mutate(ANTALL = replace_na(ANTALL, 0)) %>% 
+  # Change all numbers < 5 to zero
+  mutate(ANTALL = case_when(
+    ANTALL < 5 ~ 0,
+    .default = ANTALL
+  )) 
+
+
+# Write as csv
+write_csv(final_data, 
+          file = paste0("data/SC2_samples/", Sys.Date(), "_seqs_per_month_per_fylke.csv"))
+
+
+
+# Pangolin lineages -------------------------------------------------------
+
+# Tanker:
+# På hvilket pango-nivå skal vi legge oss?
+# Skal vi stratifisere etter fylke? Blir mye prikking kanskje?
+# Hvordan skal kategori-filen utformes? Blir det samme som fylke bare for pango?
+
+
+# UTSLUSNINGSFILENE FRA SIKKER SONE ---------------------------------------
+
+
+# Les alle utslusningsfilene fra LabWare
+files <- list.files(path = "N:/Virologi/Influensa/2223/LabwareUttrekk/", 
+                    pattern = "csv$",
+                    full.names = TRUE)
+
+# Trekk ut SARS-CoV-2 prøver som er svar ut (godkjent)
+
+df <- tibble()
+for (i in 1:length(files)) {
+
+  # Clear old objects
+  if (exists("tmp")) {
+    rm(tmp)
+  }
+  
+  # Read data files
+  x <- read_delim(files[i], delim = ";", col_names = TRUE, locale=locale(encoding="latin1")) %>% 
+    filter(`AGENS Agens` == "SARS-CoV-2") %>%
+    filter(Test_status == "A")
+  
+  # Continue only if relevant columns exist
+  if (all(c("Key", "Prøvedato2", "Fylke", "FylkeNr", "Kjønn", "Innsender", "Landsdel", "Stammenavn") %in% colnames(x))) {
+    tmp <- x %>%
+      # Select relevant columns
+      select("Key", "Prøvedato2", "Fylke", "FylkeNr", "Kjønn", "Innsender", "Landsdel", "Stammenavn") %>%
+      # Convert to character for combining later
+      mutate(Key = as.character(Key),
+             FylkeNr = as.character(FylkeNr),
+             Innsender = as.character(Innsender))
+  }
+  
+  # Combine files
+  if (exists("tmp")) {
+    df <- bind_rows(df, tmp)
+  }
+}
+
+# Remove duplicate entries (identical rows)
+df <- df %>% distinct()
+
+# Drop samples and create columns
+df_mod <- df %>% 
+  # Drop samples with no date or Fylke
+  filter(!is.na(Prøvedato2)) %>% 
+  filter(Fylke != "0") %>% 
+  # Fix date format
+  rename("PROVE_TATT" = Prøvedato2) %>% 
+  rename("FYLKE" = "Fylke") %>% 
+  # Convert to date
+  mutate(PROVE_TATT = dmy(PROVE_TATT)) %>% 
+  # Add year and month columns
+  mutate(MONTH = month(PROVE_TATT),
+         month_name = month(PROVE_TATT, label = T, abbr = T, locale = "Norwegian_Norway"),
+         YEAR = year(PROVE_TATT)) %>% 
+  unite("YEARMONTH", c(YEAR, month_name), sep = "-", remove = FALSE) %>% 
+  mutate(YEARMONTH = factor(YEARMONTH)) %>% 
+  # Remove samples with wrongly formatted dates
+  filter(!is.na(PROVE_TATT))
+
+
+# Calculate samples per month per year per Fylke and total for the country
+fylke_month <- df_mod %>% 
+  #count(MONTH, YEAR, Fylke) %>% 
+  count(YEARMONTH, FYLKE) %>% 
+  rename("ANTALL" = "n")
+
+# Calculate samples per month per year for all Fylker
+total_month <- df_mod %>% 
+  count(YEARMONTH) %>% 
+  rename("ANTALL" = "n") %>% 
+  add_column("FYLKE" = "Totalt")
+
+# Combine data
+data <- bind_rows(fylke_month, total_month) 
+
+# TODO: How and where to order the YEARMONTH column correctly? 
+# Should be independent of the data
+https://stackoverflow.com/questions/15103562/sort-year-month-column-by-year-and-month
+
+# Join with final data structure to get all possible combinations of month, year and fylke
+final_data <- left_join(final_data, data) %>% 
+  rename("ANTALL" = "n") %>% 
+  # Create new factor columns
+  mutate(
+    "MONTH"  = factor(MONTH),
+    "YEAR"   = factor(YEAR),
+    "FYLKE"  = factor(FYLKE)
+  )
+
+
+# Add flags for FHI Statistikk
+final_data <- final_data %>% 
+  add_column("SPVFLAGG" = 0)
+
+# Replace NA with 0
+final_data <- final_data %>% 
+  mutate(ANTALL = replace_na(ANTALL, 0)) 
+
+# Write as csv
+write_csv(final_data, file = "data/SC2_samples/seqs_per_month.csv")
+
+
+# Create the category file
+
+# First creating a list with the different levels necessary
+
+# Need three top levels: 
+## Dimensions
+### [[1]]
+#### Value
+#### Title
+#### Categories
+
+### [[2]]
+### [[3]]
+## MeasureTypes
+## Flags
+
+# Read example file
+example <- read_json("data/SC2_samples/seqs_per_month.category.json")
+
+my_list <- list()
+
+list(levels(final_data$YEAR))
+
+{
+  "Dimensions": [
+    {
+      "Value": "YEAR",
+      "Title": "År",
+      "Categories": [
+        {
+          "Value": "2022",
+          "Title": "2022",
+          "LevelName": "År"
+        },
+        {
+          "Value": "2023",
+          "Title": "2023",
+          "LevelName": "År"
+        }
+      ]
+    },
+    {
+      "Value": "Fylke",
+      "Title": "Fylke",
+      "Categories": [
+        {
+          "Value": "Agder",
+          "Title": "Agder",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Innlandet",
+          "Title": "Innlandet",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Møre og Romsdal",
+          "Title": "Møre og Romsdal",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Nordland",
+          "Title": "Nordland",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Oslo",
+          "Title": "Oslo",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Rogaland",
+          "Title": "Rogaland",
+          "LevelName": "Fylke"
+        },
+        {
+          "Value": "Troms og Finnmark",
+          "Title": "Troms og Finnmark",
+          "LevelName":"Fylke"
+        },
+        {
+          "Value": "Trøndelag",
+          "Title": "Trøndelag",
+          "LevelName":"Fylke"
+        },
+        {
+          "Value": "Vestfold og Telemark",
+          "Title": "Vestfold og Telemark",
+          "LevelName":"Fylke"
+        },
+        {
+          "Value": "Vestland",
+          "Title": "Vestland",
+          "LevelName":"Fylke"
+        },
+        {
+          "Value": "Viken",
+          "Title": "Viken",
+          "LevelName":"Fylke"
+        }
+      ]
+    }
+  ],
+  "MeasureTypes": [
+    {
+      "Value": "antall",
+      "Title": "Antall"
+    }
+  ],
+  "Flags": [
+    {
+      "Value": 0,
+      "Title": "",
+      "Text": ""
+    },
+    {
+      "Value": 1,
+      "Title": "..",
+      "Text": "Manglende data"
+    },
+    {
+      "Value": 2,
+      "Title": ".",
+      "Text": "Lar seg ikke beregne"
+    },
+    {
+      "Value": 3,
+      "Title": ":",
+      "Text": "Anonymisert"
+    },
+    {
+      "Value": 4,
+      "Title": "-",
+      "Text": "Data ikke tilgjengelig"
+    }
+  ]
+  }
